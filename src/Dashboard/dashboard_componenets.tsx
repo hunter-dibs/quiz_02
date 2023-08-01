6946025ec781965822df91c3093fc2cc46fcd15a
hunter-dibs <drajbongshi201403@bscse.uiu.ac.bd>
2023-08-01 09:53:21 +0600

    dashboard css added

---
+++ src/Dashboard/dashboard_componenets.tsx
@@ -0,0 +1,9 @@
+import React, { useState}   from "react";
+import './ dashboard_components.css';
+
+
+const dashboard_components = () => {
+    const [ input, UserID] = useState<string>('')
+    const [ input, UserID] = useState<string>('')
+
+}
